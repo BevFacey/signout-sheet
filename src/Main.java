c2f55480767f298fce93b2a8c3ff587a304d6cda
David Hay <misterhay@gmail.com>
2022-08-03 21:53:28 -0600

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+package src;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("starting signout program");
+        try (Stream<String> lines = Files.lines(Paths.get("students.txt"))) {
+            lines.forEach(System.out::println);
+        } catch (IOException e) {
+            System.out.println("Error reading file students.txt");
+        }
+    }
+}
